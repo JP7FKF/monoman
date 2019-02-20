a79cf9e61e2fbf293bfbc6e15b6d5ca9aacad4db
JP7FKF <jp7fkf@gmail.com>
2019-02-20 14:55:01 +0900

    add enum to software model

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,2 +1,4 @@
 class Software < ApplicationRecord
+  enum amortize_state: {unused: 0, using: 1, amortizing: 2}
+  enum asset_category: {unknown: 0, equipment: 1, small_asset:2, fixed_asset: 3}
 end
